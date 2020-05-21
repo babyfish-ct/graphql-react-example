286d2082c8c39f3e2c2746d0de56a5e0d20b7736
chentao <30338148@qq.com>
2020-05-21 15:13:26 +0800

    Use localhost

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { AppView } from './nav/AppView';
 
 function App() {
     const client = new ApolloClient({
-        uri: 'http://192.168.2.151:8080/graphql'
+        uri: 'http://localhost:8080/graphql'
     });
     return (
         <ApolloProvider client={client}>
